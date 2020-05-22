ca79d3fcd84245fff2a251026be8dc92da539197
guanxianghui001 <guanxianghui001>
2020-05-22 20:38:48 +0800

    添加代码

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -24,9 +24,18 @@ def create_app(test_config=None):
     except OSError:
         pass
 
+    from . import db
+    db.init_app(app)
     # a simple page that says hello
+    from . import auth
+    app.register_blueprint(auth.bp)
+
     @app.route('/hello')
     def hello():
         return 'Hello, World!'
+
+    from . import blog
+    app.register_blueprint(blog.bp)
+    app.add_url_rule('/', endpoint='index')
 
     return app
